99231b32a4e5d431ee5b69f3be7d02516c1ccb73
Selim <mamo31st@gmail.com>
2024-06-24 13:37:07 +0300

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+
+        classes = [BaseModel, User, State, City, Amenity, Place, Review]
+
+        if cls in classes or any(isinstance(cls, _class) for _class in classes):
+            return {key: val for key, val in FileStorage.__objects.items() if isinstance(val, cls)}
+
+        
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +38,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        if not obj:
+            return
+        k = -1
+        for key, val in FileStorage.__objects.items():
+            if obj.id == val.id:
+                k = key
+        del FileStorage.__objects[k]
 
     def reload(self):
         """Loads storage dictionary from file"""
