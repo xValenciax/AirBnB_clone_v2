e80a1f4dfa9028a324e39fc9106b3e99205ace79
Selim <mamo31st@gmail.com>
2024-06-26 03:20:46 +0300

    updates

--- models/user.py
+++ models/user.py
@@ -11,9 +11,9 @@ class User(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)        
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)        
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)        
 
     else:
         email = ''
